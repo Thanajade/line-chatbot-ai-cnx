c97fa8de863bc068a9b51ae9101739f47d14f8fc
Thanajade Dechananthachai <thanajade.d@gmail.com>
2025-02-22 21:53:25 +0700

    update cmd.sh with Firebase project setup instructions and Flex Message workshop details

--- projects/cmd.sh
+++ projects/cmd.sh
@@ -1,3 +1,37 @@
-#1 rename project name "default": "line-workshop-a4288"
-#2
+# 2025-02-22 LINE API for devloper
+
+Create firebase project via Firebase UI
+
+# install firebase-tools
+
+# set up firebase project
+
+# Add forward port using VS Code forward port tools, no more ngrok just github account URL under azure service
+
+#
+#1 rename project name "default": "line-workshop-a4288" then start line starter
+# Set project clostest to Tokyo
+# Firebase limit concurrency with maximum memory to handle client request
+
+#2 LINE project routing with security using signature
+
+# Custom Flex Message,
+
+# Create Rich Menu via LINE Manager UI
+# Create Rich Menu via Postman API
+
 npm install node-cache node-jose
+
+# Flex Message
+# https://developers.line.biz/en/docs/messaging-api/using-flex-messages/#operating-environment
+
+# Workshop: Create custom Flex message via LINE Flex simulator and Flex Formatter
+
+# Flex simulator
+# https://developers.line.biz/flex-simulator/
+# Flex formatter
+# https://next10.ex10.tech/portal/tools/flex-formatter
+
+# Postman for create LINE Rich Menu via API, more configurable than LINE Rich menu via LINE Manager UI
+
+## Create Rich Menu
